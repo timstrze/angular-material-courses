d60a97ad09c86b223bf240fc55c503bd94fa7a80
Timothy Strzelecki <timstrze@gmail.com>
2019-03-14 11:11:31 -0400

    Fix E2E tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to blueprint!');
+    expect(page.getTitleText()).toEqual('Transfer Credits Evaluation Tool');
   });
 });
